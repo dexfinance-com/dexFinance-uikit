d4d2d9c1ffb65c790bf37e0917a01a594c23e546
badgerdf <badger@gordian.agency>
2022-07-12 18:15:16 +0300

    add build

---
+++ dist/components/Svg/styles.d.ts
@@ -0,0 +1,9 @@
+export declare const StyledIconContainer: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {
+    activeBackgroundColor?: "input" | "text" | "primary" | "primaryBright" | "primaryDark" | "secondary" | "tertiary" | "success" | "failure" | "warning" | "cardBorder" | "contrast" | "dropdown" | "dropdownDeep" | "invertedContrast" | "inputSecondary" | "background" | "backgroundDisabled" | "backgroundAlt" | "backgroundAlt2" | "textDisabled" | "textSubtle" | "disabled" | "gradients" | "binance" | "overlay" | "gold" | "silver" | "bronze" | undefined;
+}, never>;
+export declare const StyledAnimatedIconComponent: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {
+    isActive: boolean;
+    height?: string | undefined;
+    width?: string | undefined;
+    hasFillIcon: boolean;
+}, never>;
---
+++ dist/components/Table/Cell.d.ts
@@ -0,0 +1,5 @@
+import { TypographyProps } from "styled-system";
+export declare const Td: import("styled-components").StyledComponent<"td", import("styled-components").DefaultTheme, TypographyProps<Required<import("styled-system").Theme<string | number>>>, never>;
+export declare const Th: import("styled-components").StyledComponent<"td", import("styled-components").DefaultTheme, TypographyProps<Required<import("styled-system").Theme<string | number>>> & {
+    as: string;
+}, "as">;
